b406a3c08569e80f47fa334695e16325d8e90f6b
DemirMasic <demir.masic@stu.ibu.edu.ba>
2025-04-06 23:26:06 +0200

    basedao and stuff

---
+++ backend/dao/UserDao.php
@@ -0,0 +1,23 @@
+<?php
+require_once __DIR__ . '/BaseDao.php';
+
+class UserDao extends BaseDao
+{
+
+    protected $table_name;
+    public function __construct()
+    {
+        $this->table_name = "user";
+        parent::__construct($this->table_name);
+    }
+
+    public function get_all()
+    {
+        return $this->query('SELECT * FROM ' . $this->table_name, []);
+    }
+
+    public function get_by_id($id)
+    {
+        return $this->query_unique('SELECT * FROM ' . $this->table_name . ' WHERE id=:id', ['id' => $id]);
+    }
+}
